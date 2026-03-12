d2942b23709056c78ba7fe70432d79766520b7a8
chirmuleadvait@gmail.com <achirmul@asu.edu>
2026-03-12 13:07:25 -0700

    v1.1.3: Minor reworks

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { StatusBar } from '@capacitor/status-bar';
+
+StatusBar.setOverlaysWebView({ overlay: false });
+
 createRoot(document.getElementById("root")!).render(<App />);
